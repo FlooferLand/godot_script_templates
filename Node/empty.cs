b595e0dcc7234e0d0e2b2be84c1a955132d36001
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2023-12-14 03:04:10 +0200

    Create empty.cs

---
+++ Node/empty.cs
@@ -0,0 +1,11 @@
+// meta-description: Empty base template
+// meta-default: true
+
+using _BINDINGS_NAMESPACE_;
+using System;
+
+using namespace Project;
+
+public partial class _CLASS_ : _BASE_ {
+_TS_
+}
